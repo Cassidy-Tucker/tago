a8174a35fffb43cc03b3894b3a02ef834dfe0ab3
Kerry Zahn <zahnkerr@Simplys-MacBook-Pro.local>
2017-12-05 18:42:32 -0700

    add new graph ver 2

--- frontend/public/js/visualization.js
+++ frontend/public/js/visualization.js
@@ -1,55 +1,53 @@
-var margin = {top: 50, right: 40, bottom: 170, left: 50},
-    width = 600 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+var margin = {
+  top: 50,
+  right: 40,
+  bottom: 130,
+  left: 60
+},
+width = 960 - margin.left - margin.right,
+height = 500 - margin.top - margin.bottom;
+
+var graphElems = {
+  colors: ["cadetblue", "darkslateblue", "dimgray", "dodgerblue", "indigo", "black"],
+  intervals: [],
+  maxActivity: 0,
+  maxAdd: function(newMax) {
+    if(newMax > graphElems.maxActivity) {
+      graphElems.maxActivity = newMax;
+    }
+  }
+}
 
 var x = d3.scaleTime().range([0, width]);
 var y = d3.scaleLinear().range([height, 0]);
 var y1 = d3.scaleLinear().range([height, 0]);
+var legendRectSize = 18;
+var legendSpacing = 4;
 
-var svg = d3.select("body").append("svg")
+var svg = d3.select("#graph-container").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
+    .style("background-color", "white")
   .append("g")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
 
 $.getJSON("./data/sample.json", function(data, error) {
-
-  var colors = ["cadetblue", "darkslateblue", "dimgray", "dodgerblue", "indigo", "black"];
-
-  // var maxActivity = 0;
-  //       data.forEach(function(set) {
-  //           set.forEach(function(d){
-  //               console.log(d.activity);
-  //               if(d.activity > maxActivity)
-  //                 maxActivity = d.activity;
-  //
-  //               set.date = +d.dateCreated;
-  //           })
-  //       });
+  //var colors = ["cadetblue", "darkslateblue", "dimgray", "dodgerblue", "indigo", "black"];
 
   for (var i = 0; i < data.length; i++) {
-
-    data[i] = data[i].intervals;
-    console.log(data[i]);
-    data[i].map(function(d) {
+    var intervals = data[i].intervals;
+    intervals.map(function(d) {
+      d.date = +d.dateCreated;
+      d.activity = +d.activity;
+      graphElems.maxAdd(d.activity);
       var dc = new Date(d.dateCreated);
-
-      d.dateCreated=dc;
-    })
-
-    data[i].forEach(function(d) {
-        d.date = +d.dateCreated;
-        d.activity = +d.activity;
+      d.dateCreated = dc;
     });
 
-    x.domain(d3.extent(data[i], function(d) { return d.date; }));
-    y.domain([0, d3.max(data[i], function(d) { return Math.max(d.activity); })]);
-    y1.domain([0, d3.max(data[i], function(d) { return Math.max(d.activity); })]);
-
-    // x.domain(d3.extent(data, function(d) { return d.date; }));
-    // y.domain([0, maxActivity]);
-    // y1.domain([0, d3.max(data, function(d) { return Math.max(d.activity); })]);
+    x.domain(d3.extent(intervals, function(d) { return d.date; }));
+    y.domain([0, graphElems.maxActivity]);
+    y1.domain([0, graphElems.maxActivity]);
 
     var valueline = d3.line()
         .x(function(d) { return x(d.date); })
@@ -58,17 +56,41 @@ $.getJSON("./data/sample.json", function(data, error) {
     console.log(data[i]);
 
     svg.append("path")
-        .data([data[i]])
+        .data([intervals])
         .attr("class", "line")
         .attr("fill", "none")
-        .style("stroke", colors[i])
+        .style("stroke", graphElems.colors[i])
         .style("stroke-width", 2.5)
         .attr("d", valueline);
 
+    // var legend = svg.selectAll('.legend')
+    //   .data([data[i]])
+    //   .enter()
+    //   .append("g")
+    //   .attr("class", "legend")
+    //   .attr("transform", function(d, i) {
+    //     var height = legendRectSize + legendSpacing;
+    //     var offset = height * graphElems.colors[i].length / 2;
+    //     var horz = -2 * legendRectSize;
+    //     var vert = i * height - offset;
+    //     return "translate(" + horz + ", " + vert + ")";
+    //   });
+    //
+    // legend.append("rect")
+    //   .attr("width", legendRectSize)
+    //   .attr("height", legendRectSize)
+    //   .style("fill", colors)
+    //   .style("stroke", colors);
+    //
+    // legend.append("text")
+    //   .attr("x", legendRectSize + legendSpacing)
+    //   .attr("y", legendRectSize - legendSpacing)
+    //   .text(function(d) {return d; });
+
     svg.selectAll("dot")
-     .data(data[i])
+     .data(intervals)
      .enter().append("circle")
-        .style("fill", colors[i])
+        .style("fill", graphElems.colors[i])
          .attr("r", 4)
          .attr("cx", function(d) { return x(d.date); })
          .attr("cy", function(d) { return y(d.activity); });
@@ -88,7 +110,7 @@ $.getJSON("./data/sample.json", function(data, error) {
   svg.append("text")
   .attr("transform",
         "translate(" + (width/2) + " ," +
-                       (height + margin.top + 35) + ")")
+                       (height + margin.top + 40) + ")")
       .attr("font-size", "20px")
       .style("text-anchor", "middle")
       .text("Time");
@@ -112,17 +134,8 @@ $.getJSON("./data/sample.json", function(data, error) {
       .text("Activity");
 
   svg.append("text")
-      .attr("x", width / 2)
-      .attr("y", 1)
-      .attr("text-anchor", "middle")
-      .style("font-size", "24px")
-      .style("text-decoration", "underline")
-      .attr("class", "shadow")
-      .text("Activity vs. Time Graph");
-
-  svg.append("text")
       .attr("x", (width / 2))
-      .attr("y", 1)
+      .attr("y", 10)
       .attr("text-anchor", "middle")
       .style("font-size", "24px")
       .style("text-decoration", "underline")
