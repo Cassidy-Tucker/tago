dcd12259b086d836d385f1b2abacd52a05bd6b96
Keesha Cain <keeshacain@Keeshas-MacBook-Pro.local>
2017-12-07 18:47:06 -0700

    routes?

--- backend/src/routes/domain.js
+++ backend/src/routes/domain.js
@@ -1,111 +1,122 @@
-const express = require ('express');
-const mongoose = require('mongoose');
-mongoose.Promise = require('bluebird');
+const express = require("express");
+const mongoose = require("mongoose");
+mongoose.Promise = require("bluebird");
 const app = express();
 
 const router = express.Router();
 
-const Domain = require('../models/domain');
-const Heatmap = require('../models/heatmap')
-const Zone = require('../models/zone')
-mongoose.connect("mongodb://Keesha:skool16@ds113826.mlab.com:13826/tago", {useMongoClient: true});
+const Domain = require("../models/domain");
+const Heatmap = require("../models/heatmap");
+const Zone = require("../models/zone");
+mongoose.connect("mongodb://Keesha:skool16@ds113826.mlab.com:13826/tago", {
+	useMongoClient: true
+});
 
-router.route('/domain')
-  .get((req, res) => {
-    Domain.find((err,domain) => {
-      if(err)
-        res.send(err);
-      res.json(domain)
-  })
-})
+router.route("/domain").get((req, res) => {
+	Domain.find((err, domain) => {
+		if (err) res.send(err);
+		res.json(domain);
+	});
+});
 
-router.route('/domain/id/:domain_id')
-  .get((req,res) => {
-    Domain.findById(req.params.domain_id, (err, domain) => {
-      if(err)
-      res.send(err);
-      res.json(domain);
-  })
-})
+router.route("/domain/id/:domain_id").get((req, res) => {
+	Domain.findById(req.params.domain_id, (err, domain) => {
+		if (err) res.send(err);
+		res.json(domain);
+	});
+});
 
-router.route('/domain/date/:domain_dateCreated')
-.get((req,res) => {
-  Domain.findOne({'dateCreated' : req.params.domain_dateCreated}, (err, domain) => {
-    if(err)
-    res.send(err);
-    res.json(domain);
-  })
-})
+router.route("/domain/date/:domain_dateCreated").get((req, res) => {
+	Domain.findOne(
+		{ dateCreated: req.params.domain_dateCreated },
+		(err, domain) => {
+			if (err) res.send(err);
+			res.json(domain);
+		}
+	);
+});
 
-router.route('/domain/current/:interval')
-.get((req,res) => {
-  var promise = Domain.findOne().sort({"dateCreated": -1}).exec();
-
-  promise.then((domain) => {
-    console.log(domain)
-  })
-  .then(domain => {
-    var promisto = Heatmap.findById(domain.id).exec();
-    promisto.then((heatmap) => {
-      console.log(heatmap)
-    })
-    .catch((err) => {
-      console.log('error:',err)
-    });
-  })
-
-  .catch((err) => {
-    console.log('error:',err)
-  });
+router.route("/domain/current/:date").get((req, res) => {
+  /* Would like to search by date and get all zones with all intervals actached to the zones to then be pushed into a graph
+    x Basic route functionality
+    Get the date, pass date into route and return date from route
+  */
+    res.json("This worked!!")
 
 
-  //   var sortHeatMaps = domain.heatmaps.sort((a,b)=>{
-  //     return a.dateCreated - b.dateCreated;
-  //   })
+
+	// var promise = Domain.findOne()
+	// 	.sort({ dateCreated: -1 })
+	// 	.exec();
+	// promise
+	// 	.then(domain => {
+	// 		console.log(domain);
+	// 	})
+	// 	.then(domain => {
+	// 		var promiseTo = Heatmap.findById(domain.id).exec();
+	// 		promiseTo
+	// 			.then(heatmap => {
+	// 				console.log(heatmap);
+	// 			})
+	// 			.catch((err) => {
+	// 				console.log("error", err);
+	// 			});
+	// 	})
+	// 	.catch((err) => {
+	// 		console.log("error", err);
+	// 	});
+
+	// var sortHeatMaps = domain.heatmaps.sort((a, b) => {
+	// 	return a.dateCreated - b.dateCreated;
+	// });
   //
-  //   var result = sortHeatMaps.pop();
+	// var result = sortHeatMaps.pop();
+	// if (domain.heatmaps.length == 0) {
+	// 	res.json({ error: "occured" });
+	// }
   //
-  //   if(domain.heatmaps.length == 0){
-  //     res.json({'error':"occured"})
-  //   }
-  //
-
-  //   var zoneArr = [];
-  //   for(var i = 0; i < domain.zones.length; i++){
-  //     Zone.findById(domain.zones[i].zoneId,(err,z)=>{
-  //       zoneArr.push(z);
-  //       if(i === domain.zones.length){
-  //         res.json(zoneArr)
-  //       }
+	// var promiseMore = Heatmap.findById(result.id).exec();
+  //   promiseMore
+  //     .then((heatmap) => {
+  //       console.log(heatmap)
+  //     })
+  //     .catch((err) => {
+  //       console.log("error",err);
   //     });
-  //   }
+// 	var zoneArr = [];
+// 	for (var i = 0; i < domain.zones.length; i++) {
+// 		Zone.findById(domain.zones[i].zoneId, (err, z) => {
+// 			zoneArr.push(z);
+// 			if (i === domain.zones.length) {
+// 				res.json(zoneArr);
+// 			}
+// 		});
+// 	}
+// 	res.json(domain);
+ });
 
-  //  res.json(domain);
-})
+router.route("/domain/query/:query_value").get((req, res) => {
+	const queryValue = req.params.query_value;
+	let query = {};
 
+	if (isNaN(Number(queryValue))) {
+		query = {
+			$or: [
+				{ name: queryValue },
+				{ description: { $regex: RegExp(queryValue) } }
+			]
+		};
+	} else {
+		query = {
+			$or: [{ dateCreated: Number(queryValue) }]
+		};
+	}
 
-router.route('/domain/query/:query_value')
-.get((req, res) => {
-  const queryValue = req.params.query_value;
-  let query = {}
+	Domain.find(query, (err, domain) => {
+		if (err) console.log(err);
 
-  if(isNaN(Number(queryValue))) {
-    query = { $or : [
-        { name : queryValue },
-        { description : { $regex : RegExp(queryValue) }}
-    ]};
-  } else {
-    query = { $or : [
-        { dateCreated : Number(queryValue) }
-    ]};
-  }
-
-  Domain.find(query, (err, domain) => {
-    if(err)
-      console.log(err);
-
-    res.json(domain);
-  });
+		res.json(domain);
+	});
 });
 
 module.exports = router;
