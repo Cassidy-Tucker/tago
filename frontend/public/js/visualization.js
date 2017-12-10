14f455d45025af812ccb08ce42f895c7c612e77b
Cassidy <macair@Cassidys-MacBook-Air.local>
2017-12-09 17:35:40 -0700

    fixed all the stuff

--- frontend/public/js/visualization.js
+++ frontend/public/js/visualization.js
@@ -31,6 +31,7 @@ $(function() {
       "black"
     ],
     intervals: [],
+    zones: [],
     maxActivity: 0
   }
   console.log("hello!!!!!!!!!!!!Are you working!??!?!?!?")
@@ -74,8 +75,6 @@ $(function() {
         }
       }
 
-
-
       for (var i = 0; i < domain.zones.length; i++) {
         graphElems.intervals = domain.zones[i].intervals;
         graphElems.intervals.map(function(d) {
@@ -84,6 +83,8 @@ $(function() {
           var dc = new Date(d.dateCreated);
           d.dateCreated = dc;
         });
+        graphElems.zones = domain.zones;
+        console.log("this is zones:", graphElems.zones);
 
         x.domain(d3.extent(graphElems.intervals, function(d) { return d.date; }));
         y.domain([0, graphElems.maxActivity]);
@@ -104,14 +105,14 @@ $(function() {
           .data(graphElems.intervals)
           .enter().append("circle")
              .style("fill", graphElems.colors[i])
-              .attr("r", 4)
+              .attr("r", 6)
               .attr("cx", function(d) { return x(d.date); })
               .attr("cy", function(d) { return y(d.activity); })
               .on("mouseover", function(d) {
                 div.transition()
                   .duration(200)
                   .style("opacity", .9);
-                div.html(Math.round(d.activity))
+                div.html(d.activity.toFixed(2))
                   .style("left", (d3.event.pageX) + "px")
                   .style("top", (d3.event.pageY - 28) + "px");
               })
@@ -122,11 +123,9 @@ $(function() {
               });
       }
         console.log(graphElems);
-    /******************************************************************************/
-
     // here we are creating our legend group, and positioning it.
       var legend = svg.selectAll('.legend')
-        .data(domain) //we pass in data, which consists of objects representing zones
+        .data(graphElems.zones) //we pass in data, which consists of objects representing zones
         .enter()
         .append("g")
         .attr("class", "legend")
@@ -159,8 +158,6 @@ $(function() {
         .attr("x", legendRectSize + legendSpacing)
         .attr("y", legendRectSize - legendSpacing)
         .text(function(d) {return d.name; });
-
-    /******************************************************************************/
 
       svg.append("g")
           .attr("class", "axis")
@@ -204,32 +201,6 @@ $(function() {
     }
   });
 });
-
-// d3.request("http://localhost:3001/api/domain/current/:span", function(error, response) {
-//   console.log("this is the data:", response);
-//   for (var i = 0; i < data.length; i++) {
-//     var intervals = data[i].intervals;
-//     intervals.map(function(d) {
-//       d.date = +d.dateCreated;
-//       d.activity = +d.activity;
-//       var dc = new Date(d.dateCreated);
-//       d.dateCreated = dc;
-//     });
-//
-//     x.domain(d3.extent(intervals, function(d) { return d.date; }));
-//     y.domain([0, graphElems.maxActivity]);
-//
-//     var valueline = d3.line()
-//         .x(function(d) { return x(d.date); })
-//         .y(function(d) { return y(d.activity); });
-//
-//     svg.append("path")
-//         .data([intervals])
-//         .attr("class", "line")
-//         .attr("fill", "none")
-//         .style("stroke", graphElems.colors[i])
-//         .style("stroke-width", 2.5)
-//         .attr("d", valueline);
 //
 //      svg.selectAll("dot")
 //       .data(intervals)
@@ -251,86 +222,3 @@ $(function() {
 //               .duration(500)
 //               .style("opacity", 0);
 //           });
-//   }
-//
-// /******************************************************************************/
-//
-// // here we are creating our legend group, and positioning it.
-//   var legend = svg.selectAll('.legend')
-//     .data(data) //we pass in data, which consists of objects representing zones
-//     .enter()
-//     .append("g")
-//     .attr("class", "legend")
-//     .attr("background-color", "yellow")
-//     .attr("transform", function(d, i) {
-//       var height = legendRectSize + legendSpacing;
-//       var offset = -380;
-//       var horz = -2 * legendRectSize;
-//       var vert = (i * height) - offset;
-//       return "translate(" + horz + ", " + vert + ")";
-//     });
-//
-// // now, we are inserting colored rectangles for each of our zones
-//   legend.append("rect")
-//     .attr("width", legendRectSize)
-//     .attr("height", legendRectSize)
-//     .style("fill", function(d,i){
-//         //here we are using this function to LOOP through 'data'. This allows
-//         //us to access the index 'i', and get the correct color for each zone.
-//         return graphElems.colors[i];
-//     })
-//     .style("stroke", function(d,i){
-//         //this works the same as above
-//         return graphElems.colors[i];
-//     });
-//
-// // now, we generate the text for our legend. Within the 'data' object, there is
-// // a key value pair for 'name', which we are accessing below on 141
-//   legend.append("text")
-//     .attr("x", legendRectSize + legendSpacing)
-//     .attr("y", legendRectSize - legendSpacing)
-//     .text(function(d) {return d.name; });
-//
-// /******************************************************************************/
-//
-//   svg.append("g")
-//       .attr("class", "axis")
-//       .attr("transform", "translate(0," + height + ")")
-//       .call(d3.axisBottom(x)
-//               .tickFormat(d3.timeFormat("%X")))
-//       .selectAll("text")
-//         .style("text-anchor", "end")
-//         .attr("dx", "-.8em")
-//         .attr("dy", ".15em")
-//         .attr("transform", "rotate(-65)");
-//
-//   svg.append("text")
-//     .attr("transform",
-//         "translate(" + (width/2) + " ," +
-//                        (height + margin.top + 40) + ")")
-//       .attr("font-size", "20px")
-//       .style("text-anchor", "middle")
-//       .text("Time");
-//
-//   svg.append("g")
-//       .attr("class", "axisSteelBlue")
-//       .call(d3.axisLeft(y));
-//
-//   svg.append("text")
-//       .attr("transform", "rotate(-90)")
-//       .attr("y", 0 - margin.left)
-//       .attr("x", 0 - (height / 2))
-//       .attr("dy", "1em")
-//       .attr("font-size", "20px")
-//       .style("text-anchor", "middle")
-//       .text("Activity");
-//
-//   svg.append("text")
-//       .attr("x", (width / 2))
-//       .attr("y", 10)
-//       .attr("text-anchor", "middle")
-//       .style("font-size", "24px")
-//       .style("text-decoration", "underline")
-//       .text("Activity vs. Time Graph");
-//
-// })
